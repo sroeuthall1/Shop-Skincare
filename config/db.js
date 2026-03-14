03d3d5993d31e6e78470487c78718f33992832ce
sroeuthall1 <142596996+sroeuthall1@users.noreply.github.com>
2026-03-14 12:12:46 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,24 @@
-const mysql = require("mysql2/promise");
+// const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const dbconn = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+const { Sequelize } = require("sequelize");
 
-module.exports = dbconn;
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || 'mysql://root:NIABxiTNuqTCFjemfhQNCJNKbkPvaXDX@mysql.railway.internal:3306/railway',
+  {
+    dialect: 'mysql'
+  }
+);
+
+
+// const dbconn = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+// });
+
+module.exports = sequelize;
